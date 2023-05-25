328b067dd29f1a96dae908a644d84d97a50b1097
user <1279061086@qq.com>
2023-05-25 18:13:08 +0800

    bash update

--- bash/nr3d.sh
+++ bash/nr3d.sh
@@ -4,8 +4,8 @@ LOG_NAME=$1
 
 CUDA_VISIBLE_DEVICES=${CUDA_ID} 
 python train.py\
-        -scannet-file /data1/liyixuan/data/sr3d_scannet/keep_all_points_with_global_scan_alignment.pkl\
-        -referit3D-file /data1/liyixuan/data/sr3d/sr3d.csv\
+        -scannet-file /data1/liyixuan/referit_my/referit3d/data/scannet/preprocess/keep_all_points_00_view_with_global_scan_alignment.pkl\
+        -referit3D-file /data1/liyixuan/referit_my/referit3d/data/language/nr3d/nr3d.csv\
         --log-dir /home/user/liyixuan/R2G/log\
         --n-workers 8\
         --batch-size 64\
--- bash/sr3d.sh
+++ bash/sr3d.sh
@@ -2,10 +2,10 @@
 CUDA_ID=$0
 LOG_NAME=$1
 
-CUDA_VISIBLE_DEVICES=${CUDA_ID} 
+CUDA_VISIBLE_DEVICES=2
 python train.py\
-        -scannet-file /data1/liyixuan/referit_my/referit3d/data/scannet/preprocess/keep_all_points_00_view_with_global_scan_alignment.pkl\
-        -referit3D-file /data1/liyixuan/referit_my/referit3d/data/language/nr3d/nr3d.csv\
+        -scannet-file /data1/liyixuan/data/sr3d_scannet/keep_all_points_with_global_scan_alignment.pkl\
+        -referit3D-file /data1/liyixuan/data/sr3d/sr3d.csv\
         --log-dir /home/user/liyixuan/R2G/log\
         --n-workers 8\
         --batch-size 64\
